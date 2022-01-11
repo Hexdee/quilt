04ba2f041b8d3c42a428bb0a0241a883a8c45024
Lord_Evendim <bartezy123@gmail.com>
2022-01-11 20:40:47 +0100

    feat: change username using token

--- quilt-contracts/contracts/KeyStorage.sol
+++ quilt-contracts/contracts/KeyStorage.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract KeyStorage {
     address private owner;
+    IERC20 private quiltToken;
+    uint256 usernameChangeFee = 10 * 10**18;
 
     struct Point {
         uint256 x;
@@ -12,11 +15,34 @@ contract KeyStorage {
     }
 
     mapping(address => Point) public usersToKeys;
+    mapping(address => string) public usernames;
 
     event KeyPublished(address publisher);
+    event UsernameChanged(address userAddress, string username);
 
-    constructor() {
+    constructor(address quiltTokenAddress) {
         owner = msg.sender;
+        quiltToken = IERC20(quiltTokenAddress);
+    }
+
+    function setUsername(string calldata newUsername) public {
+        uint256 allowedValue = quiltToken.allowance(msg.sender, address(this));
+
+        require(allowedValue >= usernameChangeFee, "not enough tokens");
+
+        quiltToken.transferFrom(msg.sender, address(this), usernameChangeFee);
+
+        usernames[msg.sender] = newUsername;
+
+        emit UsernameChanged(msg.sender, newUsername);
+    }
+
+    function getUsername(address userAddress)
+        public
+        view
+        returns (string memory)
+    {
+        return usernames[userAddress];
     }
 
     function setUserKey(uint256 _x, uint256 _y) public {
